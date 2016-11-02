4f177087642a15c09070c0618318eebdebd93a84
kangtiancheng <kangtiancheng001@163.com>
2016-11-02 17:30:37 +0800

    图片操作工具类

--- src/main/java/org/lengyan/currency/common/utils/ImgUtils.java
+++ src/main/java/org/lengyan/currency/common/utils/ImgUtils.java
@@ -5,20 +5,118 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
 
 /**
  * 图片操作工具类
+ * @since  2016年11月2日
  * @author ktc
- * 2016年11月2日 下午3:13:52
  */
 public class ImgUtils {
-	public static void main(String[] args) throws FileNotFoundException, IOException {  
-        File picture = new File("C:/Users/Dell/Downloads/152114779941825751.jpg");  
-        BufferedImage sourceImg =ImageIO.read(new FileInputStream(picture));   
-        System.out.println(String.format("%.1f",picture.length()/1024.0));  
-        System.out.println(sourceImg.getWidth());
-        System.out.println(sourceImg.getHeight());
+	
+	/**
+	 * 获取图片文件大小
+	 * @param
+	 * @return 
+	 * @变更记录 2016年11月2日 下午3:24:08  ktc
+	 */
+	public static double getImgFileSize(File file) {
+		if (file != null) {
+			return file.length()/1024.0;
+		}
+		return 0;
+	}
+	
+	/**
+	 * 获取图片文件大小
+	 * @param
+	 * @return 
+	 * @throws IOException 
+	 * @throws IllegalStateException 
+	 * @变更记录 2016年11月2日 下午3:24:08  ktc
+	 */
+	public static double getImgStreamSize(File file) throws IllegalStateException, IOException {
+		double size = 0;
+		if (file != null) {
+			InputStream in = new FileInputStream(file);
+			size = in.available()/1024.0;
+			in.close();
+		}
+		return size;
+	}
+	
+	/**
+	 * 获取图片文件宽度
+	 * @param
+	 * @return 
+	 * @变更记录 2016年11月2日 下午3:26:08  ktc
+	 */
+	public static Integer getImgWidth(File file) throws FileNotFoundException, IOException {
+		BufferedImage sourceImg =ImageIO.read(new FileInputStream(file));
+		return sourceImg.getWidth();
+	}
+	
+	/**
+	 * 获取图片文件长度
+	 * @param
+	 * @return 
+	 * @变更记录 2016年11月2日 下午3:26:08  ktc
+	 */
+	public static Integer getImgHeight(File file) throws FileNotFoundException, IOException {
+		BufferedImage sourceImg =ImageIO.read(new FileInputStream(file));
+		return sourceImg.getHeight();
+	}
+	
+	/**
+	 * 判断文件是否是图片
+	 * @param
+	 * @return 
+	 * @变更记录 2016年11月2日 下午3:35:25  ktc
+	 */
+	public static boolean isImageByStream(File file) {
+        boolean flag = false;
+        try {
+        	ImageInputStream is = ImageIO.createImageInputStream(file);
+        	if(null == is) {
+        		return flag;
+        	}
+        	is.close();
+        	flag = true;
+        } catch (Exception e) {
+        	
+        }
+        return flag;
     }
+	
+	/**
+	 * 判断文件是否是图片
+	 * @param
+	 * @return 
+	 * @变更记录 2016年11月2日 下午3:35:25  ktc
+	 */
+	public static boolean isImage(File file) {
+        boolean flag = false;
+        try {
+        	String[] imgSuffix = new String[]{".jpg", ".jpeg", ".png", ".bmp"};
+        	String fileName = file.getName();
+        	for (String suffix : imgSuffix) {
+        		if (fileName.endsWith(suffix)) {
+					flag = true;
+					break;
+				}
+			}
+        } catch (Exception e) {
+        	
+        }
+        return flag;
+    }
+	
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+    }
+
 }
