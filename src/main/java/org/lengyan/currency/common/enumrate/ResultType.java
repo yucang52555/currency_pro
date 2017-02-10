d3112a164450faf72d43c42f2cfb8ccdb5ab2bd7
kangtiancheng <kangtiancheng001@163.com>
2017-02-10 09:53:39 +0800

    增加错误异常结果类型

--- src/main/java/org/lengyan/currency/common/enumrate/ResultType.java
+++ src/main/java/org/lengyan/currency/common/enumrate/ResultType.java
@@ -6,7 +6,11 @@ package org.lengyan.currency.common.enumrate;
  * 2016年10月19日 下午1:34:11
  */
 public enum ResultType {
-    SUCCESS("0"), BUSINESS_ERROR("-1"), PARAM_ERROR("-2");
+    SUCCESS("0"), 
+    BUSINESS_ERROR("-1"), 
+    PARAM_ERROR("-2"),
+    SQL_ERROR("-3");
+	
     private String code;
 
     private ResultType(String code) {
